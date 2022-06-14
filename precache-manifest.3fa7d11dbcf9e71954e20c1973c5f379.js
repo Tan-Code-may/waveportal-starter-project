b3e723762ef73e19ba04ab6a197f75689b25e91a
Tanmay Garg <tanmayg4218@gmail.com>
2022-06-14 17:44:21 +0530

    Updates

---
+++ precache-manifest.3fa7d11dbcf9e71954e20c1973c5f379.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e456d0d5021de0f6c93a89555f55389",
+    "url": "/waveportal-starter-project/index.html"
+  },
+  {
+    "revision": "0bb0ba7e7a09c371487f",
+    "url": "/waveportal-starter-project/static/css/main.6b93457f.chunk.css"
+  },
+  {
+    "revision": "c23d5974e2945618490f",
+    "url": "/waveportal-starter-project/static/js/2.89a5c1db.chunk.js"
+  },
+  {
+    "revision": "0bb0ba7e7a09c371487f",
+    "url": "/waveportal-starter-project/static/js/main.248717db.chunk.js"
+  },
+  {
+    "revision": "d2c30444697d6b7ec9e7",
+    "url": "/waveportal-starter-project/static/js/runtime-main.60c1dfd9.js"
+  }
+]);
